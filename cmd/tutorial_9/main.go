faccae107c04480621ad435b977cbede0076d7c7
Camille Clayton <camilleclayton89@gmail.com>
2024-11-12 02:00:21 +0000

    Add chicken adv Channel

--- cmd/tutorial_9/main.go
+++ cmd/tutorial_9/main.go
@@ -2,15 +2,52 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
-// Channels + Goroutines
+// Advanced Channel
+var MAX_CHICKEN_PRICE float32 = 5
+
 func main() {
-	var c = make(chan int)
+	var chickenChannel = make(chan string)
+	var websites = []string{"walmart.com", "costco.com", "wholefoods.com"}
+	for i := range websites {
+		go checkChickenPrices(websites[i], chickenChannel)
+	}
+	sendMessage(chickenChannel)
+}
+
+func checkChickenPrices(website string, chickenChannel chan string) {
+	for {
+		time.Sleep(time.Second * 1)
+		var chickenPrice = rand.Float32() * 20
+		if chickenPrice <= MAX_CHICKEN_PRICE {
+			chickenChannel <- website
+			break
+		}
+	}
+}
+
+func sendMessage(chickenChannel chan string) {
+	fmt.Printf("\nFound a deal on chicken at %s", <-chickenChannel)
+}
+
+// Channels + Goroutines
+/*func main() {
+	var c = make(chan int, 5)
 	go process(c)
-	fmt.Println(<-c)
+	for i := range c {
+		fmt.Println(i)
+	}
 }
 
 func process(c chan int) {
-	c <- 123
-}
+	// using defer to tell the function to close the channel right before it exits, vs making sure to add it at the last line
+	defer close(c)
+	for i := 0; i < 5; i++ {
+		c <- i
+	}
+	fmt.Println("Exiting the process")
+	// close(c)
+}*/
