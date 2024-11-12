fb8bf49e811be97d9f852a1546cbffa1579fa173
Camille Clayton <camilleclayton89@gmail.com>
2024-11-12 01:06:45 +0000

    MUTEX basic

--- cmd/tutorial_8/main.go
+++ cmd/tutorial_8/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 // Lesson is goroutines
+var m = sync.Mutex{}
 var wg = sync.WaitGroup{}
 var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
 var results = []string{}
@@ -29,6 +30,8 @@ func dbCall(i int) {
 	var delay float32 = rand.Float32() * 2000
 	time.Sleep(time.Duration(delay) * time.Millisecond)
 	fmt.Println("The result from the database is:", dbData[i])
+	m.Lock()
 	results = append(results, dbData[i])
+	m.Unlock()
 	wg.Done()
 }
