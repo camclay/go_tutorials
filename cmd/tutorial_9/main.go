268e2d69aec4dc20a5dd125604f54fed8a8b034d
Camille Clayton <camilleclayton89@gmail.com>
2024-11-12 02:09:03 +0000

    Added tofu!

--- cmd/tutorial_9/main.go
+++ cmd/tutorial_9/main.go
@@ -8,14 +8,28 @@ import (
 
 // Advanced Channel
 var MAX_CHICKEN_PRICE float32 = 5
+var MAX_TOFU_PRICE float32 = 3
 
 func main() {
 	var chickenChannel = make(chan string)
+	var tofuChannel = make(chan string)
 	var websites = []string{"walmart.com", "costco.com", "wholefoods.com"}
 	for i := range websites {
 		go checkChickenPrices(websites[i], chickenChannel)
+		go checkTofuPrices(websites[i], tofuChannel)
 	}
-	sendMessage(chickenChannel)
+	sendMessage(chickenChannel, tofuChannel)
+}
+
+func checkTofuPrices(website string, c chan string) {
+	for {
+		time.Sleep(time.Second * 1)
+		var tofu_price = rand.Float32() * 20
+		if tofu_price < MAX_TOFU_PRICE {
+			c <- website
+			break
+		}
+	}
 }
 
 func checkChickenPrices(website string, chickenChannel chan string) {
@@ -29,8 +43,14 @@ func checkChickenPrices(website string, chickenChannel chan string) {
 	}
 }
 
-func sendMessage(chickenChannel chan string) {
-	fmt.Printf("\nFound a deal on chicken at %s", <-chickenChannel)
+func sendMessage(chickenChannel chan string, tofuChannel chan string) {
+	select {
+	case website := <-chickenChannel:
+		fmt.Printf("\nText Sent: Found deal on chicken at %v.", website)
+	case website := <-tofuChannel:
+		fmt.Printf("\nEmail Sent: Found deal on tofu at %v.", website)
+	}
+	//fmt.Printf("\nFound a deal on chicken at %s", <-chickenChannel)
 }
 
 // Channels + Goroutines
